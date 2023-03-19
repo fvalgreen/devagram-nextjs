45622ce891370e61259869397afe80be70daa470
Felipe Valverde <62726786+fvalgreen@users.noreply.github.com>
2023-03-19 19:05:45 -0300

    Configuração Cosmic e multer

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,38 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
-import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
-import md5 from 'md5';
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
+import md5 from "md5";
 import { UsuarioModel } from "@/models/UsuarioModel";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import { LoginResposta } from "@/types/LoginResposta";
 
-
 const endpointLogin = async (
-    req : NextApiRequest,
-    res : NextApiResponse<RespostaPadraoMsg | any>
- ) => {
+  req: NextApiRequest,
+  res: NextApiResponse<RespostaPadraoMsg | any>
+) => {
+  const { MINHA_CHAVE_JWT } = process.env;
+  if (!MINHA_CHAVE_JWT) {
+    return res.status(500).json({ erro: "ENV JWT não informada" });
+  }
 
-    const {MINHA_CHAVE_JWT} = process.env;
-    if(!MINHA_CHAVE_JWT){
-        return res.status(500).json({erro: 'ENV JWT não informada'})
+  if (req.method === "POST") {
+    const { login, senha } = req.body;
+    const usuariosEncontrados = await UsuarioModel.find({
+      email: login,
+      senha: md5(senha),
+    });
+    if (usuariosEncontrados && usuariosEncontrados.length > 0) {
+      const usuarioEncontrado = usuariosEncontrados[0];
+
+      const token = jwt.sign({ id: usuarioEncontrado._id }, MINHA_CHAVE_JWT);
+
+      return res.status(200).json({
+        nome: usuarioEncontrado.nome,
+        email: usuarioEncontrado.email,
+        token,
+      });
     }
-
-    if(req.method === 'POST'){
-        const {login, senha} = req.body;
-        const usuariosEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)})
-        if(usuariosEncontrados && usuariosEncontrados.length > 0){
-            const usuarioEncontrado = usuariosEncontrados[0];
-            
-            const token = jwt.sign({id : usuarioEncontrado._id}, MINHA_CHAVE_JWT);
-
-            return res.status(200).json({
-                nome : usuarioEncontrado.nome,
-                email : usuarioEncontrado.email, 
-                token})
-        }
-        return res.status(405).json({erro : 'Usuário ou senha não encontrado'})
-    }
-    return res.status(405).json({erro : 'Metodo informado não é válido'})
-}
+    return res.status(405).json({ erro: "Usuário ou senha não encontrado" });
+  }
+  return res.status(405).json({ erro: "Metodo informado não é válido" });
+};
 
 export default conectarMongoDB(endpointLogin);
--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -2,8 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
 
 const usuarioEndPoint = (req: NextApiRequest, res: NextApiResponse) => {
-
-    return res.status(200).json('Usuário autenticado com sucesso')
-}
+  return res.status(200).json("Usuário autenticado com sucesso");
+};
 
 export default validarTokenJWT(usuarioEndPoint);
