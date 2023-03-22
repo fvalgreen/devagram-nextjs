1486cf16306d1719403487f93032b9ecd7927e50
Felipe Valverde <62726786+fvalgreen@users.noreply.github.com>
2023-03-22 20:53:47 -0300

    api comentario

---
+++ pages/api/comentario.ts
@@ -0,0 +1,47 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { RespostaPadraoMsg } from "@/types/RespostaPadraoMsg";
+import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
+import { UsuarioModel } from "@/models/UsuarioModel";
+import { PublicacaoModel } from "@/models/PublicacaoModel";
+import { SeguidorModel } from "@/models/SeguidorModel";
+import { conectarMongoDB } from "@/middlewares/conectarMongoDB";
+
+const comentarioEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>)=>{
+    try {
+        if(req.method === 'PUT'){
+
+            const {userID, id} = req.query;
+            const usuarioLogado = await UsuarioModel.findById(userID);
+            
+            if(!usuarioLogado){
+                return res.status(400).json({erro: 'Usuário não encontrado'});
+            };
+            
+            const publicacao = await PublicacaoModel.findById(id);
+
+            if(!publicacao){
+                return res.status(400).json({erro: 'Publicação não encontrada'});
+            }
+
+            if(!req.body || !req.body.comentario || req.body.comentario.length < 2){
+                return res.status(400).json({erro: 'Comentário não é válido'});
+            }
+            
+            const comentario = {
+                usuarioId: usuarioLogado._id,
+                nome: usuarioLogado.nome,
+                comentario: req.body.comentario
+            }
+            publicacao.comentarios.push(comentario);
+            await PublicacaoModel.findByIdAndUpdate({_id: publicacao._id}, publicacao);
+            return res.status(200).json({msg: 'Comentário adicionado com sucesso'})
+
+        }
+        return res.status(405).json({erro: 'Método informado não é válido'})
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({erro: 'Ocorreu um erro ao comentar a publicação'})
+    }
+}
+
+export default validarTokenJWT(conectarMongoDB(comentarioEndpoint))
