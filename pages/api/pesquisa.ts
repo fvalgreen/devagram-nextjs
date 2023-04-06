71da9beda9eb6ac720fa00fb6d99a72e3a51b837
Felipe Valverde <62726786+fvalgreen@users.noreply.github.com>
2023-04-06 09:11:12 -0300

    Correção no segueEsseUsuario

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -4,18 +4,35 @@ import { validarTokenJWT } from "@/middlewares/validarTokenJWT"; // Importando o
 import { conectarMongoDB } from "@/middlewares/conectarMongoDB"; // Importando o middleware de conexão com DB que foi criado
 import type { RespostaPadraoMsg } from "@/types/RespostaPadraoMsg"; // Importando o tipo de resposta padrão que criamos
 import { UsuarioModel } from "@/models/UsuarioModel"; // Importando o model do Usuário
+import { SeguidorModel } from "@/models/SeguidorModel";
 
 
 const pesquisaEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any[]>) => {
     try {
         if(req.method === "GET"){ // Verificando o método HTTP do request caso seja algo diferente de GET retornará que o método não é válido
             if(req?.query?.id){ // Verifica se há um request e se esse request possui uma propriedade id. Esse caso é para uma pesquisa por ID
-                const usuariosEncontrado = await UsuarioModel.findById(req?.query?.id); // Busca na DB o usuário usando o id
-                usuariosEncontrado.senha = null; // Define a senha do usuário como Null pra não ficar trafegando a senha do usuário
-                if(!usuariosEncontrado){ // Caso não retorne um usuário retorna um erro de usuário não encontrado
+                const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id); // Busca na DB o usuário usando o id
+                usuarioEncontrado.senha = null; // Define a senha do usuário como Null pra não ficar trafegando a senha do usuário
+                if(!usuarioEncontrado){ // Caso não retorne um usuário retorna um erro de usuário não encontrado
                     return res.status(400).json({erro: 'Usuário não encontrado'})
                 }
-                return res.status(200).json(usuariosEncontrado); // Retorna o usuário encontrado pelo id
+                const user = {
+                    senha: null,
+                    segueEsseUsuario: false,
+                    nome: usuarioEncontrado.nome,
+                    email: usuarioEncontrado.email,
+                    _id: usuarioEncontrado._id,
+                    avatar: usuarioEncontrado.avatar,
+                    seguidores: usuarioEncontrado.seguidores,
+                    seguindo: usuarioEncontrado.seguindo,
+                    publicacoes: usuarioEncontrado.publicacoes,
+                } as any;
+
+                const segueEsseUsuario = await SeguidorModel.find({ usuarioId: req?.query?.userID, usuarioSeguidoId: usuarioEncontrado._id });
+                if (segueEsseUsuario && segueEsseUsuario.length > 0) {
+                    user.segueEsseUsuario = true;
+                }
+                return res.status(200).json(user); // Retorna o usuário encontrado pelo id
             }else{ // Caso não seja passado nenhum id pelo query faremos a busca pelo filtro
                 const {filtro} = req.query; // Usando um destructor pegamos a propriedade filtro da query da request
 
